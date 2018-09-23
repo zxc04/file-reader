27cc2a422685ecbebec6f85797f21967fc7405de
zxc04 <40673732+zxc04@users.noreply.github.com>
2018-09-23 02:10:05 +0200

    Console App (#9)

---
+++ src/FileReader.ConsoleApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using FileReader.Common;
+using FileReader.EncryptionProviders;
+using FileReader.RoleProviders;
+using System;
+
+namespace FileReader.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+
+            while (!done)
+            {
+                string input;
+                FileTypes fileType = FileTypes.Text;
+                bool isEncrypted = false;
+                bool useRoles = false;
+                string role = null;
+                string path = null;
+
+                Console.Write("File type (0 = text, 1 = xml, 2 = json): ");
+                input = Console.ReadLine();
+                fileType = (FileTypes)int.Parse(input);
+
+                Console.Write("Use encryption (y = Yes, n = No)? ");
+                input = Console.ReadLine();
+                isEncrypted = input == "y";
+
+                if (isEncrypted)
+                {
+                }
+
+                Console.Write("Use role based security (y = Yes, n = No)? ");
+                input = Console.ReadLine();
+                useRoles = input == "y";
+
+                if (useRoles)
+                {
+                    Console.Write("Enter your role: ");
+                    input = Console.ReadLine();
+                    role = input;
+                }
+
+                Console.Write("Enter file path: ");
+                input = Console.ReadLine();
+                path = input;
+
+                Console.WriteLine($"Reading file {path}");
+
+                string content = ReadFile(fileType, path, isEncrypted, useRoles, role);
+                Console.WriteLine(content);
+
+                Console.Write("Do you want to read another file (y = Yes, n = No)? ");
+                input = Console.ReadLine();
+                done = input == "n";
+            }
+        }
+
+        static string ReadFile(FileTypes fileType, string path, bool isEncrypted, bool useRoles, string role)
+        {
+            IEncryptionProvider encryptionProvider = null;
+            IRoleProvider roleProvider = null;
+
+            if (isEncrypted)
+                encryptionProvider = new ReverseEncryption();
+
+            if (useRoles)
+                roleProvider = new SimpleRoleProvider();
+
+            Reader reader = new Reader(encryptionProvider, roleProvider);
+
+            try
+            {
+                return reader.ReadFile(fileType, path, isEncrypted: isEncrypted, role: role);
+            }
+            catch(UnauthorizedAccessException)
+            {
+                return $"ERROR: Role \"{role}\" is not authorized to access this file";
+            }
+        }
+    }
+}
